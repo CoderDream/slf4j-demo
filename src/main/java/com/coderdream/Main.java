62d57efaeefc63bbff7df6f0c678e013051e96bf
CoderDream <coderdream@gmail.com>
2024-11-25 11:01:49 +0800

    初始化版本

--- src/main/java/com/coderdream/Main.java
+++ src/main/java/com/coderdream/Main.java
@@ -1,40 +1,3 @@
-# slf4j-demo
-
-### 踩坑相关踩坑
-
-1. 引用调用SLF4J API 运行报错
-
-   ```none
-   SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
-   SLF4J: Defaulting to no-operation (NOP) logger implementation
-   SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
-   ```
-
-   答：SLF4J只是接口层，未找到实现则默认的空实现(nop)【1.6 开始】，并打印警告
-
-   可引入 SLF4J 实现/绑定层，如：
-
-   ```none
-   <dependency>
-   	<groupId>ch.qos.logback</groupId>
-   	<artifactId>logback-classic</artifactId>
-   	<version>1.2.3</version>
-   	<scop>test</scop>
-   </dependency>
-   ```
-   这个版本一定要选1.2.3，不要升级到1.4.12，否则报错，而且不输出日志
-2. 在多个参数情况下，若想打印 Throwable 堆栈信息，需注意Throwable必须放在最后一个
-   例：logger.error("错误消息：{}",e.getMessage(),e);
-
-![image.png](README/1732498539244-edf49d7a-578c-4e50-826a-0b8e5ad57b16.png)
-
-
-
-### 最佳实践
-
-#### 源码
-
-```java
 package com.coderdream;
 
 import lombok.extern.slf4j.Slf4j;
@@ -69,9 +32,3 @@ public class Main {
 
     }
 }
-
-```
-
-#### 输出
-
-![image-20241125095101197](README/image-20241125095101197.png)
